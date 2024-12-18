af235be694c75404afe412a57eb8b5a31c7fa52c
Nadav Dori <dori.nadav@gmail.com>
2024-12-18 08:20:15 +0200

    add the option to add activities to an existing priodic agenda1

--- src/app/cmps/dashboard/DashboardMenu.tsx
+++ src/app/cmps/dashboard/DashboardMenu.tsx
@@ -17,8 +17,8 @@ export default function DashboardMenu({ }: Props) {
     return (
         <section className='dashboard-menu-container flex-col flex-jc-ac'>
             <p onClick={()=>handelRoute('/dashboard/presence')}>דף נוכחות</p>
-            <p  onClick={()=>{handelRoute('/dashboard/create_periodic_agenda')}}>יצירת לוז תקופתי</p>
-            <p  onClick={()=>{handelRoute('/dashboard/edit_periodic_agenda')}}>עריכת לוז נוכחי</p>
+            <p  onClick={()=>{handelRoute('/dashboard/create_periodic_agenda')}}>יצירת/עריכת לוז תקופתי</p>
+            {/* <p  onClick={()=>{handelRoute('/dashboard/edit_periodic_agenda')}}>עריכת לוז נוכחי</p> */}
             <p  onClick={()=>{handelRoute('/dashboard/practitioners_list')}}>רשימת תלמידים</p>
              <p onClick={()=>{handelRoute('/dashboard/create_announcement')}}>פרסום של לוח מודעות</p>
             {/* <p>סיכום רכישות </p>
