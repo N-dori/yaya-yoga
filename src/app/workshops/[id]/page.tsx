36938b4bee385e09a95b229c25b0b053749cb54b
Nadav Dori <dori.nadav@gmail.com>
2024-12-18 09:06:50 +0200

    add the option to add activities to an existing priodic agenda3

--- src/app/workshops/[id]/page.tsx
+++ src/app/workshops/[id]/page.tsx
@@ -12,7 +12,10 @@ const workshop = await getWorkshop(params.id)
     <h1 className='title tac mt-1'>YAYA YOGA</h1>
       <h2 className='mb-1 tac'>סדנאות</h2>
      { workshop&&<WorkshopsPreview workshop={workshop} isDetailsMode={true}/>}
+     <div className='flex-jc-ac'>
+      <button type='button' className='btn'>להרשמה</button>
 
+     </div>
     </main>
   )
 }
